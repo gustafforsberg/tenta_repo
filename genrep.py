061d223ea7cc09a4bca09eada0b548512fa2993a
gustafforsberg <123724047+gustafforsberg@users.noreply.github.com>
2023-10-11 13:59:11 +0200

    genrep

---
+++ genrep.py
@@ -0,0 +1,26 @@
+
+
+
+print('Beräknar differansen ac jämna/udda tal....')
+
+
+
+udda_lista = []
+jamn_lista = []
+
+
+for i in range(1, 2001, 2):
+   udda_lista.append(i)
+
+
+
+for i in range(2, 2002, 2):
+   jamn_lista.append(i)
+
+summa_udda = sum(udda_lista)
+summa_jamn = sum(jamn_lista)
+
+
+
+
+print(f'Differancen = {summa_jamn - summa_udda}')
